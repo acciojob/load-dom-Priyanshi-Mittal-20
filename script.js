401de2b9d487a1c00d8e1ddda0c3d9de883e29f0
Priyanshi-Mittal-20 <priyanshimzn05@gmail.com>
2026-07-07 13:14:52 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 //your JS code here. If required.
+document.addEventListener("DOMContentLoaded",display)
+function display()
+{
+	let text=document.createElement("p")
+	text.textContent="DOM load success"
+	document.body.appendChild(text)
+}
